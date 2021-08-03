ed02a38932920a0d44d6310a96d38cd012bf8020
“[Kimberly <codinglikeagirl42@gmail.com>
2021-08-03 07:59:28 -0700

    Added sessions, MVP

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 import random
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+app.secret_key = 'ThisIsNotARealSecretKey'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    
     if request.method == 'POST':
-        guess =request.form['guess']
+        guess =int(request.form['guess'])
+        magic_number = int(session['magic_number'])
+        still_guessing = True
         if guess < magic_number:
-            low_value = guess
+            session['low_value'] = guess
             message = f"(guess) is too low!"
         elif guess > magic_number:
-            high_value = guess
+            session['high_value'] = guess
             message = f"(guess) is too high!"
         elif guess == magic_number:
             message = f"Congratulation (guess) is correct!"
         else:    
             message = "Please enter a valid input"
-        still_guessing = True
+        
     else:
-        low_value = 1
-        high_value = 50
-        magic_number = random.randint(low_value, high_value)
+        session["low_value"] = 1
+        session["high_value"]= 50
+        session["magic_number"] = random.randint(session['low_value'], session['high_value'])
         still_guessing = True
 
         message = ''
 
-    return render_template('index.html', message = message,
-        low_value = low_value, high_value = high_value, still_guessing = still_guessing)
+    return render_template('index.html', message = message, still_guessing = still_guessing)
 
 if __name__ == '__main__':
     app.run()
